82ff375bb37161913d23684d2bc2fe1691e87aa6
Richard Michael <rmichael@edgeofthenet.org>
2012-09-02 14:28:57 +0200

    Initial commit ; rails new with capistrano and capified.

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source :rubygems
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+gem 'jquery-rails'
+
+gem 'capistrano'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infrastructureapp::Application.config.secret_token = '99b61de0659ba2dc031228fca58c64ef032bf19bae305c91d7bf2a7a1e2dc28220b70fddf7eef1b1a7975cc37f33a87070057a7de13c7b1a79249a01fc279e18'
